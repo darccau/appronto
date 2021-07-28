4235231be56b17aa77fb7b625a9728c988124044
duardopx <darccau@protonmail.com>
2021-07-27 23:21:33 -0300

    fix: remove trash configurations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// const multer = require('multer')
+// const multerConfig = require('./config/multer')
 const router = express.Router()
 
 const patientController = require('./controllers/patientController')
@@ -10,5 +12,9 @@ router.post('/patients', patientController.create)
 router.put('/patients/:id', patientController.update)
 
 router.delete('/patients/:id', patientController.delete)
+
+// router.post('/reports', multer(multerConfig).single('file'), () => {
+//   console.log({"fuck": "that shit"})
+// })
 
 module.exports = router
