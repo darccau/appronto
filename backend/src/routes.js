ae72f91a9922d5bf4b4d79754e58e31dc0901d67
duardopx <darccau@protonmail.com>
2021-08-01 08:44:51 -0300

    fix: multer configuration export

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-// const multer = require('multer')
-// const multerConfig = require('./config/multer')
-const router = express.Router()
+const multer = require('multer')
+const multerConfig = require('../config/multer.js')
 
 const patientController = require('./controllers/patientController')
+
+const router = express.Router()
+const upload = multer(multerConfig)
 
 router.get('/patients', patientController.index)
 
@@ -13,8 +15,6 @@ router.put('/patients/:id', patientController.update)
 
 router.delete('/patients/:id', patientController.delete)
 
-// router.post('/reports', multer(multerConfig).single('file'), () => {
-//   console.log({"fuck": "that shit"})
-// })
+router.post('/patients/reports', upload.array('reports'), patientController.upload)
 
 module.exports = router
