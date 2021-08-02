121960f8608e8073d273b4ae411f02f12fe0f477
duardopx <darccau@protonmail.com>
2021-08-01 22:05:05 -0300

    feat: add cors package

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const cors = require('cors') 
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
 app.use(routes)
 
 app.listen(1337, () => {
   console.log('[*] Server listen on port 1337')
 })
+
