bd7853faded8f4b4d960fe0a935cf4bf98848127
duardopx <darccau@protonmail.com>
2021-09-03 10:40:06 -0300

    feat: update routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
+import CreateUser from './pages/CreateUser';
 import Orphanage from './pages/Orphanage';
 
 export default function Routes() {
@@ -10,8 +11,10 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/orphanages/create" component={CreateOrphanage} />
-        <Route path="/orphanages/:id" component={Orphanage} />
+        <Route path="/appointment/create" component={CreateOrphanage} />
+        <Route path="/user/create" component={CreateUser} />
+        <Route path="/user/:id" component={Orphanage} />
+        <Route path="/appointment/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
