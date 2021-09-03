fd7b54169a1e505eed59ed2162a5e49ff5e17510
duardopx <darccau@protonmail.com>
2021-09-03 10:46:52 -0300

    feat: add retrive appointment information page

--- frontend/src/pages/Orphanage/index.tsx
+++ frontend/src/pages/Orphanage/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
+import axios from "axios";
 
 import './styles.css';
 
+const retriveAppointments = ()  => {
+  axios.post('http://localhost:1337/appointment/appointments', {
+      "id_user":"1"
+      }).then(response => {console.log(response.data)})
+}
+
+retriveAppointments()
 
 export default function Orphanage() {
   return (
@@ -11,7 +19,6 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
 
           <div className="images">
             <button className="active" type="button">
@@ -36,7 +43,8 @@ export default function Orphanage() {
           
           <div className="orphanage-details-content">
             <h1>Lar das meninas</h1>
-            <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p>
+            <p>Presta assistência a crianças de 06 a 15 anos que se encontre em
+            situação de risco e/ou vulnerabilidade social.</p>
 
             <hr />
 
@@ -45,10 +53,10 @@ export default function Orphanage() {
 
             <div className="open-details">
               <div className="hour">
-              bla blabla bla blabla bla bla bla bla bla bla bla bla
-              bla blabla bla blabla bla bla bla bla bla bla bla bla
-              bla blabla bla blabla bla bla bla bla bla bla bla bla
-              bla blabla bla blabla bla bla bla bla bla bla bla bla
+              bla blabla bla  
+              bla blabla bla  
+              bla blabla bla  
+              bla blabla bla  
               </div>
             </div>
 
