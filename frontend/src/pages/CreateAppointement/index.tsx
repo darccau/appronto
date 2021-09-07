dc72336f40d0df274f1157cd740d5328db9f1b3f
duardopx <darccau@protonmail.com>
2021-09-07 15:12:32 -0300

    chore: rename diretori to CreateAppointement

--- frontend/src/pages/CreateAppointement/index.tsx
+++ frontend/src/pages/CreateAppointement/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from 'axios';
+// import axios from 'axios';
 
 import PrimaryButton from "../../components/PrimaryButton";
 import Sidebar from "../../components/Sidebar";
@@ -7,33 +7,46 @@ import Sidebar from "../../components/Sidebar";
 import './styles.css';
 import { FiPlus } from "react-icons/fi";
 
-const fuck = axios.get('http://localhost:1337/test')
-fuck.then(response => {
-  console.log(response.data)
-  })
+// const fuck = axios.get('http://localhost:1337/test')
+// fuck.then(response => {
+//   console.log(response.data)
+//   })
+
 
 
 export default function OrphanagesMap() {
+
+//  const [name, setName] = useState("");
+//  const [email, setEmail] = useState("");
+//  const [message, setMessage] = useState("");
+//
+//  function handleSubmit(event) {
+//    event.preventDefault();
+//    console.log("name", name);
+//    console.log("email", email);
+//    console.log("message", message);
+//  }
+
   return (
       <div id="page-create-orphanage">
       <Sidebar />
       <main>
-      <form className="create-orphanage-form">
+      <form className="create-orphanage-form" >
       <fieldset>
-      <legend>Create Appointment</legend>
+      <legend>Relatar consulta</legend>
       <div className="input-block">
-      <label htmlFor="name">Doctor name</label>
+      <label htmlFor="name">Nome do médico</label>
       <input id="name" />
       </div>
 
 
       <div className="input-block">
-      <label htmlFor="name">Appointment type</label>
+      <label htmlFor="name">Tipo de consulta</label>
       <input id="name" />
       </div>
 
             <div className="input-block">
-              <label htmlFor="about">Note</label>
+              <label htmlFor="about">Notas</label>
               <textarea id="name" maxLength={300} />
             </div>
 
@@ -43,14 +56,13 @@ export default function OrphanagesMap() {
               <div className="uploaded-image">
 
               </div>
-
               <button className="new-image">
                 <FiPlus size={24} color="#15b6d6" />
               </button>
             </div>
           </fieldset>
 
-          <PrimaryButton type="submit">Submit</PrimaryButton>
+          <PrimaryButton type="submit">Enviar</PrimaryButton>
         </form>
       </main>
     </div>
