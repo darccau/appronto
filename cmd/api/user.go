f3a0f7769a133ab9480269dc602cd2cd6c4f66ee
darccau <darccau@kali.darccau>
2022-10-16 11:42:04 -0300

    feat: remove sensitive data on error messages

--- cmd/api/user.go
+++ cmd/api/user.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/darccau/appronto/internal/data"
 )
+
+func (app *application) createUser(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Password  string `json:"password"`
+		Email     string `json:"email"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+}
 
 func (app *application) showUserHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIdParam(r)
