5e09c07cc446478a41443999bc0483c44ed9e1b1
darccau <darccau@gmail.com>
2023-03-02 21:50:44 -0300

    feat: implement rate limit by ip

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/user/:id", app.updateUser)
 	router.HandlerFunc(http.MethodDelete, "/v1/user/:id", app.deleteUser)
 
-	return app.rateLimit(app.recoverPanic(router))
+	return app.recoverPanic(app.rateLimit(router))
 }
