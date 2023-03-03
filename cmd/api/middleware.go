d3edfe0a4152db4ac02eb5d9d2f742b28059373d
darccau <darccau@gmail.com>
2023-03-03 13:28:09 -0300

    feat: implementing rate limit map growth control

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -23,30 +24,54 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-  var (
-    mu sync.Mutex
-    clients = make(map[string]* rate.Limiter)
-  )
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    ip,_, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil{
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
 			app.serverErrorResponse(w, r, err)
-      return
+			return
 		}
 
-    mu.Lock()
+		mu.Lock()
 
-    if _, found := clients[ip]; !found {
-      clients[ip] = rate.NewLimiter(2, 4)
-    }
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
 
-    if !clients[ip].Allow() {
-      mu.Unlock()
-      app.rateLimitExceededResponse(w,r)
-      return
-    }
-    mu.Unlock()
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
