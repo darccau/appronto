2764bb952fc400ede5903faf9e727bd42903f6e6
darccau <darccau@gmail.com>
2023-03-22 23:25:00 -0300

    feat: update email template

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,11 +10,13 @@ var ErrRecordNotFound = errors.New("record not found")
 type Models struct {
 	Appointments AppointmentModel
 	Users        UserModel
+	Tokens       TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Users:        UserModel{DB: db},
 		Appointments: AppointmentModel{DB: db},
+		Tokens:       TokenModel{DB: db},
 	}
 }
